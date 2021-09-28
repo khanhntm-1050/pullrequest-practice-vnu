045426a3b77a7d7806d0a4672104920300bf9de4
khangneko <khangt893@gmail.com>
2021-09-29 01:09:15 +0700

    03_Webプログラミング(基礎) 課題

--- kadai02.rb
+++ kadai02.rb
@@ -1,4 +1,12 @@
 # 好きな数値で代入してください
 number = gets.to_i
-
+if number %15 == 0
+    puts "15の倍数ですと出力する。"
+elsif number % 3 == 0
+    puts "3の倍数ですと出力する。"
+elseif number % 5 == 0
+    puts "5の倍数ですと出力する。"
+else 
+    puts "3の倍数でも5の倍数でもありませんと出力する。"
+end
 # 条件分岐を作成してください
--- kadai03.rb
+++ kadai03.rb
@@ -1,4 +1,16 @@
 # 好きな年を西暦（例：2000）で代入してください
 year = gets.to_i #gets(標準入力)
-
+cancel_year = [1916, 1940, 1944, 2020]
+if year < 1896
+    puts "#{year}年にオリンピック䛿まだありません"
+elsif year % 4 == 0
+    if cancel_year.include? year
+        puts "#{year}年にオリンピック䛿開催されませんでした"
+    else
+        puts "#{year}年にオリンピック䛿開催されました"
+    end
+else
+    puts "#{year}年䛿夏季オリンピックイヤーで䛿ありません"
+end
+        
 # 以下の条件分岐に、新しい条件を追加してください
