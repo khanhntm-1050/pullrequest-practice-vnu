f576804de61e6faf0c6de99da60d2d79ec4a0cfe
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2021-10-07 21:07:06 +0700

    add pr

--- kadai03.rb
+++ kadai03.rb
@@ -2,3 +2,14 @@
 year = gets.to_i #gets(標準入力)
 
 # 以下の条件分岐に、新しい条件を追加してください
+cancelYears = [1916, 1940, 1944, 2020]
+
+if year <= 1985
+  puts "#{year}年にオリンピックはまだありません"
+elsif cancelYears.include? year
+  puts "#{year}年にオリンピックは開催されませんでした"
+elsif year % 4 == 0
+  puts "#{year}年にオリンピックは開催されました"
+else
+  puts "#{year}年䛿夏季オリンピックイヤーで䛿ありません"
+end
