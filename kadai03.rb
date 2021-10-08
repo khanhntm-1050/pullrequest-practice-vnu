62388783698338d18a32ad94a3d5a4932a8be3d3
Đại Phạm Trọng <daiphamtrong@ais-MacBook-Pro-9.local>
2021-10-08 15:17:09 +0700

    kadai 3,4,5

--- kadai03.rb
+++ kadai03.rb
@@ -2,3 +2,14 @@
 year = gets.to_i #gets(標準入力)
 
 # 以下の条件分岐に、新しい条件を追加してください
+notOrganizedYears = [1916, 1940, 1944, 2020]
+
+if year < 1896
+    puts "#{year}年にオリンピックはまだありません"
+  elsif notOrganizedYears.include? year
+    puts "#{year}年にオリンピックは開催されませんでした"
+  elsif year % 4 == 0
+    puts "#{year}年にオリンピックはあります"
+  else
+    puts "#{year}年䛿夏季オリンピックイヤーではありません"
+  end
