7d5b67e8da6d126069036aa3ad133d059ab8a876
Đại Phạm Trọng <daiphamtrong@ais-MacBook-Pro-9.local>
2021-10-08 11:22:34 +0700

    Do exercise 6,7,8

--- kadai01.rb
+++ kadai01.rb
@@ -1,7 +1,8 @@
 # このプログラムを修正して、正しく表示されるようにしてください
 number = rand(1..3) * 100
 puts "あなたの得点は#{number}ポイントです"
-if number = 300
+if number == 300
   puts "おめでとう！"
 else
   puts "ざんねん！"
+end
--- kadai02.rb
+++ kadai02.rb
@@ -2,3 +2,12 @@
 number = gets.to_i
 
 # 条件分岐を作成してください
+if number % 3 == 0 && number % 5 == 0
+    puts "15の倍数です！"
+elsif number % 3 == 0
+  puts "3の倍数です！"
+elsif number % 5 == 0
+  puts "5の倍数です！"
+else
+  puts "3の倍数でも5の倍数でもありませんと！"
+end
